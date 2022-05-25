eb72049080c8bcea28a288554cdbfc538ba59b77
darioVargas91 <vargasdario91@gmail.com>
2022-05-25 15:58:37 -0500

    fix: fix test

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,18 +7,8 @@ import {
   HttpStatus,
   Body,
 } from '@nestjs/common';
-import { CreateProductDto } from './dto/product.dto';
-import { ProductService } from './product.service';
 @Controller('product')
 export class ProductController {
-  constructor(private productService: ProductService) {}
+  constructor() {}
 
-  @Post('/create')
-  async createPost(@Res() res, @Body() createProductDto: CreateProductDto) {
-    const product = await this.productService.createProduct(createProductDto);
-    return res.status(HttpStatus.OK).json({
-      message: 'received',
-      product: product,
-    });
-  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -7,16 +7,7 @@ import { Product } from './interfaces/product.interface';
 @Injectable()
 export class ProductService {
   constructor(
-    @InjectModel('Product') private readonly productModel: Model<Product>,
+   
   ) {}
 
-  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
-    const product = new this.productModel(createProductDto);
-    return await product.save();
-  }
-
-  async getProducts(): Promise<Product[]> {
-    const products = await this.productModel.find();
-    return products;
-  }
 }
